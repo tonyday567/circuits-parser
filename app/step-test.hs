b519e9458958e30aed25d89558112160d2e4a13e
Tony Day <tonyday567@gmail.com>
2026-06-17 05:37:11 +1000

    style: hlint fixes

--- app/step-test.hs
+++ app/step-test.hs
@@ -4,31 +4,31 @@ import Circuit.Parser.Step
 import Control.Applicative ((<|>))
 
 -- Test helpers
-p_a, p_b, p_ab, p_offer :: Parser String Char String
-p_a = char 'a' $> "a-parsed"
-p_b = char 'b' $> "b-parsed"
-p_ab = char 'a' >> char 'b' $> "ab-parsed"
-p_offer = char 'a' >> offer "got-a"
+pA, pB, pAb, pOffer :: Parser String Char String
+pA = char 'a' $> "a-parsed"
+pB = char 'b' $> "b-parsed"
+pAb = char 'a' >> char 'b' $> "ab-parsed"
+pOffer = char 'a' >> offer "got-a"
 
 main :: IO ()
 main = do
   putStrLn "=== char 'a' on \"abc\" ==="
-  print $ run p_a "abc"
+  print $ run pA "abc"
 
   putStrLn "=== char 'a' on \"xyz\" ==="
-  print $ run p_a "xyz"
+  print $ run pA "xyz"
 
   putStrLn "=== char 'a' >> char 'b' on \"ax\" ==="
-  print $ run p_ab "ax"
+  print $ run pAb "ax"
 
-  putStrLn "=== p_b <|> p_a on \"abc\" ==="
-  print $ run (p_b <|> p_a) "abc"
+  putStrLn "=== pB <|> pA on \"abc\" ==="
+  print $ run (pB <|> pA) "abc"
 
-  putStrLn "=== p_ab <|> p_a on \"ax\" ==="
-  print $ run (p_ab <|> p_a) "ax"
+  putStrLn "=== pAb <|> pA on \"ax\" ==="
+  print $ run (pAb <|> pA) "ax"
 
-  putStrLn "=== (p_offer >> p_b) on \"ab\" ==="
-  print $ run (p_offer >> p_b) "ab"
+  putStrLn "=== (pOffer >> pB) on \"ab\" ==="
+  print $ run (pOffer >> pB) "ab"
 
 ($>) :: (Functor f) => f a -> b -> f b
 ($>) = flip (<$)
